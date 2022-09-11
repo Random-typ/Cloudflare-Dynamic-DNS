ce5b56ffa91c264b3736eae289a615e95f356263
Random Typ <no.name.typ2.0@gmail.com>
2022-09-11 23:34:11 +0200

    Final changes to monitor.

--- Monitor/Form1.Designer.cs
+++ Monitor/Form1.Designer.cs
@@ -29,11 +29,288 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label12 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.linkLabel1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Location = new System.Drawing.Point(755, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(720, 462);
+            this.panel1.TabIndex = 0;
+            this.panel1.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(637, 428);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Next";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.linkLabel1.Location = new System.Drawing.Point(133, 123);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(177, 17);
+            this.linkLabel1.TabIndex = 6;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "create a Cloudflare API token\r\n";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(32, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(559, 119);
+            this.label3.TabIndex = 4;
+            this.label3.Text = resources.GetString("label3.Text");
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(32, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(225, 30);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Cloudflare API token:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(17, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(214, 37);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Getting started";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(32, 245);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(281, 23);
+            this.textBox2.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.richTextBox1);
+            this.panel2.Controls.Add(this.label13);
+            this.panel2.Controls.Add(this.label9);
+            this.panel2.Controls.Add(this.textBox4);
+            this.panel2.Controls.Add(this.flowLayoutPanel1);
+            this.panel2.Controls.Add(this.label12);
+            this.panel2.Controls.Add(this.button5);
+            this.panel2.Controls.Add(this.label8);
+            this.panel2.Controls.Add(this.button4);
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Location = new System.Drawing.Point(1, 2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(720, 462);
+            this.panel2.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(20, 387);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 19;
+            this.button2.Text = "reload";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(379, 235);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(301, 119);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(380, 117);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(61, 15);
+            this.label13.TabIndex = 18;
+            this.label13.Text = "API token:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(682, 149);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(35, 15);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "show";
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(380, 146);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(297, 23);
+            this.textBox4.TabIndex = 14;
+            this.textBox4.UseSystemPasswordChar = true;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(20, 69);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(344, 312);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label12.Location = new System.Drawing.Point(573, 364);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(104, 17);
+            this.label12.TabIndex = 12;
+            this.label12.Text = "Status: stopped";
+            // 
+            // button5
+            // 
+            this.button5.Enabled = false;
+            this.button5.Location = new System.Drawing.Point(479, 360);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "stop";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(379, 217);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(48, 15);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Output:";
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(379, 360);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "start";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(633, 428);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "save";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(20, 25);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(181, 37);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Select zones:";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 200;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1476, 465);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cloudflare Dynamic DNS Monitor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Panel panel1;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private TextBox textBox2;
+        private Panel panel2;
+        private LinkLabel linkLabel1;
+        private Button button1;
+        private Label label8;
+        private Button button4;
+        private Button button3;
+        private Label label6;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Label label12;
+        private Button button5;
+        private Label label13;
+        private Label label9;
+        private TextBox textBox4;
+        private System.Windows.Forms.Timer timer1;
+        private RichTextBox richTextBox1;
+        private Button button2;
     }
 }
--- Monitor/Form1.cs
+++ Monitor/Form1.cs
@@ -1,10 +1,234 @@
+using System.Diagnostics;
+using System.Net;
+using System.Text;
+using System.Text.Json.Nodes;
+
+
 namespace Monitor
 {
     public partial class Form1 : Form
     {
+        public const string ConfigFile = "CloudflareDDNSConfig.json";
+
+        public void WriteOut(string _message)
+        {
+            richTextBox1.Text += _message + "\n";
+        }
+
+        public void DDNSStatus()
+        {
+            Process[] proc = Process.GetProcessesByName("Cloudflare Dynamic DNS");
+            if (proc.Length == 0)
+            {
+                button4.Enabled = true;
+                button5.Enabled = false;
+                label12.Text = "Status: stopped";
+            }
+            else
+            {
+                button4.Enabled = false;
+                button5.Enabled = true;
+                label12.Text = "Status: running";
+            }
+        }
+
+        public void LoadConfig()
+        {
+            WriteOut("Loading config...");
+            try
+            {
+                JsonNode configjson = JsonObject.Parse(File.ReadAllText(ConfigFile));
+                textBox4.Text = configjson["token"].ToString();
+
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://api.cloudflare.com/client/v4/zones");
+                request.Headers.Add("Authorization", "Bearer " + textBox4.Text);
+                request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                using (Stream stream = response.GetResponseStream())
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    JsonNode json = JsonObject.Parse(reader.ReadToEnd());
+                    foreach (var zone in json["result"].AsArray())
+                    {
+                        string id = zone["id"].ToString();
+                        for (int i = 0; i < configjson["zones"].AsArray().Count; i++)
+                        {
+                            if (configjson["zones"].AsArray()[i].ToString() == id)
+                            {
+                                AddZone(zone["name"].ToString(), id, true);
+                                break;
+                            }
+                            if (i + 1 == configjson["zones"].AsArray().Count)
+                            {
+                                AddZone(zone["name"].ToString(), id, false);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Cloudflare API token is invalid or local config file is formatted incorrectly.\n\nDetailed error: \n\n" + e.ToString(), "Error");
+                WriteOut("Error loading config.");
+                return;
+            }
+            WriteOut("Loaded config.");
+        }
+
+        public void WriteConfig()
+        {
+            string json = "{\"token\": \"" + textBox4.Text + "\", \"zones\":[";
+            foreach (var item in flowLayoutPanel1.Controls)
+            {
+                Panel panel = (Panel)item;
+                if (!((CheckBox)panel.Controls["checkbox"]).Checked)
+                {
+                    continue;
+                }
+                string id = panel.Controls["label"].Text;
+                json += "\"" + id.Substring("zone id: ".Length) + "\",";
+
+            }
+            if (json.EndsWith(','))
+            {
+                json = json.Remove(json.Length - 1);
+            }
+            json += "]}";
+            FileStream file = File.Create(ConfigFile);
+            file.Write(Encoding.ASCII.GetBytes(json), 0, json.Length);
+            file.Close();
+            WriteOut("Saved config.");
+        }
+
+        public void AddZone(string _domain, string _id, bool _checked)
+        {
+            Panel panel = new Panel();
+
+            panel.Location = new System.Drawing.Point(3, 3);
+            panel.Name = "panel";
+            panel.Size = new System.Drawing.Size(319, 67);
+            panel.TabIndex = 0;
+            panel.BorderStyle = BorderStyle.FixedSingle;
+
+            Label label = new Label();
+
+            label.AutoSize = true;
+            label.Location = new System.Drawing.Point(3, 44);
+            label.Name = "label";
+            label.Size = new System.Drawing.Size(61, 15);
+            label.TabIndex = 2;
+            label.Text = "zone id: " + _id;
+
+            CheckBox checkbox = new CheckBox();
+
+            checkbox.AutoSize = true;
+            checkbox.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            checkbox.Location = new System.Drawing.Point(3, 8);
+            checkbox.Name = "checkbox";
+            checkbox.Size = new System.Drawing.Size(109, 21);
+            checkbox.TabIndex = 1;
+            checkbox.Text = _domain;
+            checkbox.UseVisualStyleBackColor = true;
+            checkbox.Checked = _checked;
+
+            panel.Controls.Add(checkbox);
+            panel.Controls.Add(label);
+            flowLayoutPanel1.Controls.Add(panel);
+        }
+
+        public void ClearZones()
+        {
+            flowLayoutPanel1.Controls.Clear();
+            WriteOut("Cleared zones.");
+        }
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://dash.cloudflare.com/profile/api-tokens");
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+            if (label9.Text == "show")
+            {
+                label9.Text = "hide";
+                textBox4.UseSystemPasswordChar = false;
+            }
+            else if (label9.Text == "hide")
+            {
+                label9.Text = "show";
+                textBox4.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ClientSize = new Size(panel2.Size.Width + 20, panel2.Size.Height + 40);
+            MaximumSize = ClientSize;
+            MinimumSize = ClientSize;
+
+            timer1.Start();
+
+            if (!File.Exists(ConfigFile))
+            {
+                panel1.Location = panel2.Location;
+                panel1.Visible = true;
+                return;
+            }
+            LoadConfig();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DDNSStatus();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text.Length == 0)
+            {
+                return;
+            }
+
+            string json = "{\"token\": \"" + textBox2.Text + "\", \"zones\":[]}";
+            FileStream file = File.Create(ConfigFile);
+            file.Write(Encoding.ASCII.GetBytes(json), 0, json.Length);
+            file.Close();
+            panel1.Visible = false;
+            LoadConfig();
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            WriteConfig();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Process.Start("Cloudflare Dynamic DNS.exe");
+            WriteOut("Started Dynamic DNS.");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            foreach (var process in Process.GetProcessesByName("Cloudflare Dynamic DNS"))
+            {
+                process.Kill();
+            }
+            WriteOut("Stopped Dynamic DNS.");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ClearZones();
+            LoadConfig();
         }
     }
 }
